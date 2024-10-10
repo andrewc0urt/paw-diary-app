30a71c3720d49a97c857d5b0e32f3010efc97485
andrewc0urt <andrew.court.dev@gmail.com>
2024-10-10 09:48:15 -0400

    Set Up Navbar Component With Material UI And IsLoggedIn Prop
    - Implemented the Navbar component using Material UI
    
    - Customized navigation links to reflect the application requirements
    
    - Added an avatar profile image to the user menu
    
    - Integrated isLoggedIn prop for dynamic display of navigation options

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 // import "./App.css";
 
+import Navbar from "./components/Navbar";
 import SignIn from "./pages/sign-in/SignIn";
+
+const isLoggedIn = () => {
+  return true;
+};
 
 function App() {
   return (
     <>
+      <Navbar isLoggedIn={isLoggedIn()} />
       <h1>Initial Paw Diary Setup</h1>
       <SignIn />
     </>
