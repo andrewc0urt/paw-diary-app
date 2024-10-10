49300f27c2213dc6c77f438d1c99fee74e2d129e
andrewc0urt <andrew.court.dev@gmail.com>
2024-10-10 08:20:36 -0400

    Set Up Frontend Structure and Import Shared Material UI Files
    - Created components and pages folders to organize frontend structure
    
    Imported Material UI 'shared-theme' folder and files required for SignIn and other templates
    
    - Added and imported the SignIn component to begin building the authentication flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 // import "./App.css";
 
+import SignIn from "./pages/sign-in/SignIn";
+
 function App() {
   return (
     <>
       <h1>Initial Paw Diary Setup</h1>
+      <SignIn />
     </>
   );
 }
